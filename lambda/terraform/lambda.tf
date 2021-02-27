57b13a026ea7c2e8efc95245ab9c90e469b637f6
Nathan Toups <n@rjrbt.io>
2021-02-26 22:54:07 -0700

    tf: added iam role

--- lambda/terraform/lambda.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_lambda_function" "liftplan" {
-    function_name = var.project_name
-    role          = aws_iam_role.lambda.arn
-    handler       = "main"
-    runtime       = "go1.x"
-}
--- lambda/terraform/outputs.tf
+++ lambda/terraform/outputs.tf
@@ -1,9 +1,3 @@
-
-
-output "lambda_function_arn" {
-  value = aws_lambda_function.liftplan.arn
-}
-
 output "lambda_iam_role_arn" {
   value = aws_iam_role.lambda.arn
 }
