376cb348ff9d84dcd774af267038fb57051fec34
Nathan Toups <n@rjrbt.io>
2021-02-26 22:50:41 -0700

    tf: added lamdba and iam role

--- lambda/terraform/lambda.tf
+++ lambda/terraform/lambda.tf
@@ -1,4 +1,6 @@
 resource "aws_lambda_function" "liftplan" {
     function_name = var.project_name
     role          = aws_iam_role.lambda.arn
+    handler       = "main"
+    runtime       = "go1.x"
 }
