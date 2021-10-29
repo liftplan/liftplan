3c5de522163739bda80ea0ababa18bc928b1fd7c
Nathan Toups <n@rjrbt.io>
2021-10-28 22:40:57 -0600

    test: updated even more fto tests

--- strategy/fto/values_test.go
+++ strategy/fto/values_test.go
@@ -3,6 +3,8 @@ package fto
 import (
 	"net/url"
 	"testing"
+
+	"github.com/liftplan/liftplan/gear"
 )
 
 func TestIsChecked(t *testing.T) {
@@ -27,6 +29,60 @@ func TestIsChecked(t *testing.T) {
 		k, v, e := test.key, test.vals, test.expected
 		if isChecked(k, v) != e {
 			t.Errorf("key: %v, value: %v, expected: %v", k, v, e)
+		}
+	}
+}
+
+func TestFromValues(t *testing.T) {
+	t.Parallel()
+
+	m1 := Movement{
+		Name:        "deadlift",
+		TrainingMax: 175,
+		Unit:        gear.LBS,
+	}
+	m2 := Movement{
+		Name:        "bench press",
+		TrainingMax: 400,
+		Unit:        gear.LBS,
+	}
+	m3 := Movement{
+		Name:        "overhead press",
+		TrainingMax: 175,
+		Unit:        gear.LBS,
+	}
+	m4 := Movement{
+		Name:        "squat",
+		TrainingMax: 400,
+		Unit:        gear.LBS,
+	}
+	s1 := Strategy{
+		Movements: []Movement{m1, m2, m3, m4},
+		Gear:      gear.Default(gear.LBS),
+		Type:      FSLMULTI,
+		Warmup:    true,
+		JokerSets: true,
+	}
+
+	goodVals, _ := s1.Values()
+	t.Log(goodVals)
+	t.Log()
+
+	tt := []struct {
+		input    url.Values
+		expected Strategy
+		err      error
+	}{
+		{url.Values{}, Strategy{}, gear.ErrMissingUnitQuery},
+		{goodVals, s1, nil},
+	}
+
+	for _, test := range tt {
+		_, err := FromValues(test.input)
+		if err != nil {
+			if err != test.err {
+				t.Error(test.err, err)
+			}
 		}
 	}
 }
