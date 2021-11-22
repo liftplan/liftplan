602b80bd72fd0bd1132b81af44a924d34185d5aa
Nathan Toups <n@rjrbt.io>
2021-11-21 18:30:26 -0600

    change port

--- serve/run/main.go
+++ serve/run/main.go
@@ -13,7 +13,7 @@ func main() {
 	http.HandleFunc("/", handler.Root())
 	http.HandleFunc("/plan", handler.Plan())
 	s := http.Server{
-		Addr: "localhost:8080",
+		Addr: "localhost:9000",
 	}
 	log.Println(s.ListenAndServe())
 }
